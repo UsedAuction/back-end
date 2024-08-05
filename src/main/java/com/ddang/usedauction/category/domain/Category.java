0011bcc4098d77f90af9158c38326ee6add921eb
jinhwan Bea <paewhal20@gmail.com>
2024-08-05 21:28:38 +0900

    refactor: 카테고리 필드 변경
    부모 카테고리 PK를 저장하는 필드를 두어 소분류 카테고리임을 파악할 수 있도록 함

--- src/main/java/com/ddang/usedauction/category/domain/Category.java
+++ src/main/java/com/ddang/usedauction/category/domain/Category.java
@@ -1,17 +1,13 @@
 package com.ddang.usedauction.category.domain;
 
+import com.ddang.usedauction.category.dto.CategoryServiceDto;
 import com.ddang.usedauction.config.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,12 +30,8 @@ public class Category extends BaseTimeEntity {
     @Column(nullable = false)
     private String categoryName;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_category_id")
-    private Category category;
-
-    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
-    private List<Category> categoryList;
+    @Column
+    private Long parentId; // 부모 카테고리 PK
 
     @Column
     private LocalDateTime deletedAt;
