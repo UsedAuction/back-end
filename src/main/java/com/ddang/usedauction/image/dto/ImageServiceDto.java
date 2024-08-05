7027df0f4938f8097fde5f91c2484f5d76ceb4a4
jinhwan Bea <paewhal20@gmail.com>
2024-08-05 22:14:38 +0900

    feat: 이미지 서비스에서 사용할 dto

---
+++ src/main/java/com/ddang/usedauction/image/dto/ImageServiceDto.java
@@ -0,0 +1,33 @@
+package com.ddang.usedauction.image.dto;
+
+import com.ddang.usedauction.ask.dto.AskServiceDto;
+import com.ddang.usedauction.auction.dto.AuctionServiceDto;
+import com.ddang.usedauction.image.domain.ImageType;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(toBuilder = true)
+public class ImageServiceDto implements Serializable {
+
+    private Long id;
+    private String imageUrl;
+    private ImageType imageType;
+    private AskServiceDto ask;
+    private AuctionServiceDto auction;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime createdAt;
+}
