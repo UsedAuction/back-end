71683573c33a0e061b71e10b64d97ea07997d306
jinhwan Bea <paewhal20@gmail.com>
2024-08-05 23:49:22 +0900

    refactor: 경매 엔티티 필드 수정
    이미지 리스트 필드에 cascade 속성 추가

--- src/main/java/com/ddang/usedauction/auction/domain/Auction.java
+++ src/main/java/com/ddang/usedauction/auction/domain/Auction.java
@@ -103,7 +103,7 @@ public class Auction extends BaseTimeEntity {
     @OneToMany(mappedBy = "auction", fetch = FetchType.LAZY)
     private List<Ask> askList; // 문의글 리스트
 
-    @OneToMany(mappedBy = "auction", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "auction", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Image> imageList; // 이미지 리스트
 
     @Column
