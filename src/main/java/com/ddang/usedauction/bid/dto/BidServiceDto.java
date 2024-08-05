6bfe7908d504fa860a001abeec1168a2395eac1a
jinhwan Bea <paewhal20@gmail.com>
2024-08-05 22:12:11 +0900

    add: 입찰 서비스 dto에 필드 추가
    입찰한 회원 필드 추가

--- src/main/java/com/ddang/usedauction/bid/dto/BidServiceDto.java
+++ src/main/java/com/ddang/usedauction/bid/dto/BidServiceDto.java
@@ -1,6 +1,7 @@
 package com.ddang.usedauction.bid.dto;
 
 import com.ddang.usedauction.auction.dto.AuctionServiceDto;
+import com.ddang.usedauction.member.dto.MemberServiceDto;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -9,7 +10,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-// todo : 회원 dto로 추가
 // 서비스에서 사용할 dto
 @Getter
 @AllArgsConstructor
@@ -20,5 +20,6 @@ public class BidServiceDto implements Serializable {
     private Long id;
     private long bidPrice; // 입찰가
     private AuctionServiceDto auction; // 입찰한 경매
+    private MemberServiceDto member; // 입찰한 회원
     private LocalDateTime createdAt; // 생성 날짜
 }
