85fdaa84d3bc55f377191660ff11c6d8d0e34fb9
jinhwan Bea <paewhal20@gmail.com>
2024-08-06 23:49:48 +0900

    add: 에러코드 추가
    1. 즉시 구매가가 입찰 시작가보다 작은 경우의 에러코드 추가
    2. 경매가 일주일 넘도록 지속되는 경우 에러코드 추가
    3. 이미지가 6개가 넘어가는 경우 에러코드 추가

--- src/main/java/com/ddang/usedauction/auction/exception/AuctionErrorCode.java
+++ src/main/java/com/ddang/usedauction/auction/exception/AuctionErrorCode.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AuctionErrorCode {
 
+    LOW_PRICE(HttpStatus.BAD_REQUEST.value(), "즉시구매가는 입찰시작가보다 큰 수이어야 합니다."),
+    END_DATE_IS_AFTER_7(HttpStatus.BAD_REQUEST.value(), "경매는 최대 일주일동안 진행할 수 있습니다."),
+    TOO_MANY_IMAGE(HttpStatus.BAD_REQUEST.value(), "이미지는 6개까지 등록 가능합니다."),
     NOT_FOUND_AUCTION(HttpStatus.BAD_REQUEST.value(), "등록되지 않은 경매글입니다.");
 
     private final int status;
