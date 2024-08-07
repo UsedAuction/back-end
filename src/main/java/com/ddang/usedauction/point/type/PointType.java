398c1833e9f43b295922f72f481e27bafa98813f
Cathunder <102372626+Cathunder@users.noreply.github.com>
2024-08-07 18:01:00 +0900

    point type의 각 enum값에 name, description 추가

--- src/main/java/com/ddang/usedauction/point/type/PointType.java
+++ src/main/java/com/ddang/usedauction/point/type/PointType.java
@@ -1,6 +1,15 @@
 package com.ddang.usedauction.point.type;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum PointType {
-    USE,
-    CHARGE
+
+    USE("USE", "사용"),
+    CHARGE("CHARGE", "충전");
+
+    private final String name;
+    private final String description;
 }
