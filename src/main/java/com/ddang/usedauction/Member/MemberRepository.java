87911c6fead79b7ff65acbd6ed4adbe27733e7c1
sh035 <saab35@naver.com>
2024-08-07 17:40:22 +0900

    feat: chat에 필요한 repository 생성(aution, member)

---
+++ src/main/java/com/ddang/usedauction/Member/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.ddang.usedauction.Member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
