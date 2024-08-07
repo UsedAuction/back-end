490037b358144c24a79437f10a1a8711a6dc936c
Cathunder <102372626+Cathunder@users.noreply.github.com>
2024-08-07 15:53:48 +0900

    포인트 내역 조회에 필요한 member 관련 최소한의 코드 추가

---
+++ src/main/java/com/ddang/usedauction/member/MemberException.java
@@ -0,0 +1,14 @@
+package com.ddang.usedauction.member;
+
+import lombok.Getter;
+
+@Getter
+public class MemberException extends RuntimeException {
+
+    private final MemberErrorCode memberErrorCode;
+
+    public MemberException(MemberErrorCode memberErrorCode) {
+        super(memberErrorCode.getMessage());
+        this.memberErrorCode = memberErrorCode;
+    }
+}
