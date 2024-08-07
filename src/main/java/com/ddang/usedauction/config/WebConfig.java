0b539520677a253c916d9e8832f7ec9969640849
sh035 <saab35@naver.com>
2024-08-07 22:03:53 +0900

    feat: Cors 설정 추가

---
+++ src/main/java/com/ddang/usedauction/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ddang.usedauction.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("Authorization", "Content-Type")
+        .exposedHeaders("Custom-Header")
+        .maxAge(3600);
+  }
+}
