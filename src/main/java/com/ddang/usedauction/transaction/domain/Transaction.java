85b935aaa685ead043bc5f9a9a97023fa8632cf9
jinhwan Bea <paewhal20@gmail.com>
2024-08-08 21:24:55 +0900

    feat: 거래 엔티티

---
+++ src/main/java/com/ddang/usedauction/transaction/domain/Transaction.java
@@ -0,0 +1,47 @@
+package com.ddang.usedauction.transaction.domain;
+
+import com.ddang.usedauction.auction.domain.Auction;
+import com.ddang.usedauction.config.BaseTimeEntity;
+import com.ddang.usedauction.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(toBuilder = true)
+public class Transaction extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private long price; // 거래 가격
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransType transType; // 거래 타입
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member; // 회원
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "auction_id")
+    private Auction auction; // 경매
+}
