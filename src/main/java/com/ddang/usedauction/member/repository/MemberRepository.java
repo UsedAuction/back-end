8f998443ccfa34395fc36b017870652416a6ca35
jinhwan Bea <paewhal20@gmail.com>
2024-08-08 21:22:45 +0900

    feat: 이메일로 회원 조회

--- src/main/java/com/ddang/usedauction/member/repository/MemberRepository.java
+++ src/main/java/com/ddang/usedauction/member/repository/MemberRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByMemberId(String memberId); // 회원 아이디로 조회
+
+    Optional<Member> findByEmail(String email); // 회원 이메일로 조회
 }
