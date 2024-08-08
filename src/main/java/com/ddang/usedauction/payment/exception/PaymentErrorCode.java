3c262fe76277ef5be0cd0aaa75ed1255b5deaa18
Cathunder <102372626+Cathunder@users.noreply.github.com>
2024-08-09 02:35:45 +0900

    결제 준비 과정에서 금액 검증 추가, order 테이블에 tid 저장 로직 추가

--- src/main/java/com/ddang/usedauction/payment/exception/PaymentErrorCode.java
+++ src/main/java/com/ddang/usedauction/payment/exception/PaymentErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum PaymentErrorCode {
 
     INVALID_USER(HttpStatus.BAD_REQUEST.value(), "동일한 유저가 아닙니다."),
+    NOT_EQUAL_PAYMENT_AMOUNT(HttpStatus.BAD_REQUEST.value(), "요청 금액과 저장된 금액이 일치하지 않습니다."),
     PAYMENT_CANCEL(HttpStatus.BAD_REQUEST.value(), "결제가 취소되었습니다."),
     PAYMENT_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(), "결제에 실패하였습니다.")
     ;
--- src/main/java/com/ddang/usedauction/payment/service/PaymentService.java
+++ src/main/java/com/ddang/usedauction/payment/service/PaymentService.java
@@ -1,11 +1,26 @@
 package com.ddang.usedauction.payment.service;
 
+import static com.ddang.usedauction.order.exception.OrderErrorCode.NOT_FOUND_ORDER;
 import static com.ddang.usedauction.payment.exception.PaymentErrorCode.INVALID_USER;
+import static com.ddang.usedauction.payment.exception.PaymentErrorCode.NOT_EQUAL_PAYMENT_AMOUNT;
+import static com.ddang.usedauction.payment.exception.PaymentErrorCode.PAYMENT_FAIL;
+import static com.ddang.usedauction.point.type.PointType.CHARGE;
 
 import com.ddang.usedauction.member.domain.Member;
+import com.ddang.usedauction.member.exception.MemberErrorCode;
+import com.ddang.usedauction.member.exception.MemberException;
+import com.ddang.usedauction.member.repository.MemberRepository;
+import com.ddang.usedauction.order.domain.Orders;
+import com.ddang.usedauction.order.exception.OrderErrorCode;
+import com.ddang.usedauction.order.exception.OrderException;
+import com.ddang.usedauction.order.repository.OrderRepository;
+import com.ddang.usedauction.payment.dto.PaymentApproveDto;
 import com.ddang.usedauction.payment.dto.PaymentInfoDto;
 import com.ddang.usedauction.payment.dto.PaymentReadyDto;
 import com.ddang.usedauction.payment.exception.PaymentException;
+import com.ddang.usedauction.point.domain.PointHistory;
+import com.ddang.usedauction.point.repository.PointRepository;
+import com.ddang.usedauction.point.type.PointType;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,11 +29,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PaymentService {
 
     @Value("${payment.secret-key}")
@@ -31,8 +48,10 @@ public class PaymentService {
     private static final String HEADER_AUTHORIZATION = "Authorization";
     private static final String HEADER_CONTENT_TYPE = "Content-Type";
 
-//    private final MemberRepository memberRepository;
     private final RestTemplate restTemplate;
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final PointRepository pointRepository;
 
     // 결제 준비
     public PaymentReadyDto.Response ready (PaymentInfoDto.Request request) {
@@ -47,9 +66,17 @@ public class PaymentService {
             .id(1L)
             .build();
 
-        // 로그인한 유저와 요청으로 받은 유저의 id가 같은지 검증
-        if(!member.getId().equals(request.getMemberId())) {
+        Orders order = orderRepository.findByMemberId(member.getId())
+            .orElseThrow(() -> new OrderException(NOT_FOUND_ORDER));
+
+        // 로그인한 유저와 요청으로 받은 유저의 id가 동일한지 비교
+        if (!member.getId().equals(request.getMemberId())) {
             throw new PaymentException(INVALID_USER);
+        }
+
+        // db에 저장된 금액과 입력받은 금액이 동일한지 비교
+        if (order.getPrice() != request.getPrice()) {
+            throw new PaymentException(NOT_EQUAL_PAYMENT_AMOUNT);
         }
 
         // 요청으로 받은 값들을 String으로 변환
@@ -58,6 +85,7 @@ public class PaymentService {
         String itemNameStr = request.getPrice() + " 포인트"; // 포인트아이템을 db에 저장하지 않으므로 포인트값으로 아이템명을 생성함
         String priceStr = String.valueOf(request.getPrice());
 
+        // 카카오로 보낼 결제 준비 요청에 필요한 정보들 생성
         // header 설정
         HttpHeaders headers = new HttpHeaders();
         headers.set(HEADER_AUTHORIZATION, "SECRET_KEY " + SECRET_KEY);
@@ -90,6 +118,10 @@ public class PaymentService {
         );
 
         log.info("response: {}", response);
+
+        // order 테이블에 tid 저장
+        order.setTid(response.getTid());
+        orderRepository.save(order);
 
         return response;
     }
