c8ff76fa7e1e4a157ebcae9a8743e38510ee3d8f
jinhwan Bea <paewhal20@gmail.com>
2024-08-08 21:23:36 +0900

    refactor: baseEntityTime 필드명 변경으로 인한 수정

--- src/main/java/com/ddang/usedauction/point/repository/PointRepository.java
+++ src/main/java/com/ddang/usedauction/point/repository/PointRepository.java
@@ -1,6 +1,7 @@
 package com.ddang.usedauction.point.repository;
 
 import com.ddang.usedauction.point.domain.PointHistory;
+import java.time.LocalDate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,15 +9,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-
 @Repository
 public interface PointRepository extends JpaRepository<PointHistory, Long> {
 
     @Query("SELECT p FROM point_history p " +
         "WHERE p.member.email =:username " +
-        "AND p.registerDate BETWEEN :startDate AND :endDate " +
-        "ORDER BY p.registerDate ASC")
+        "AND p.createdAt BETWEEN :startDate AND :endDate " +
+        "ORDER BY p.createdAt ASC")
     Page<PointHistory> findAllPoint(
         @Param("username") String username,
         @Param("startDate") LocalDate startDate,
