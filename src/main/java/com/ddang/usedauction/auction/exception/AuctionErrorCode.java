f94ded3328a0108cf0b1b04f909ca8b2de058608
jinhwan Bea <paewhal20@gmail.com>
2024-08-08 21:20:47 +0900

    add: 에러 코드 추가
    1. 회원 포인트 부족으로 인한 구매 확정 실패 에러코드 추가
    2. 경매가 진행중일 때 구매 확정을 진행하는 경우 에러코드 추가

--- src/main/java/com/ddang/usedauction/auction/exception/AuctionErrorCode.java
+++ src/main/java/com/ddang/usedauction/auction/exception/AuctionErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AuctionErrorCode {
 
+    FAIL_CONFIRM_AUCTION_BY_BUYER(HttpStatus.BAD_REQUEST.value(), "구매 확정 실패 -> 회원 포인트가 부족합니다."),
+    CONTINUE_AUCTION(HttpStatus.BAD_REQUEST.value(), "아직 경매가 진행중입니다."),
     LOW_PRICE(HttpStatus.BAD_REQUEST.value(), "즉시구매가는 입찰시작가보다 큰 수이어야 합니다."),
     END_DATE_IS_AFTER_7(HttpStatus.BAD_REQUEST.value(), "경매는 최대 일주일동안 진행할 수 있습니다."),
     TOO_MANY_IMAGE(HttpStatus.BAD_REQUEST.value(), "이미지는 6개까지 등록 가능합니다."),
