0c368cd7eb383bbe24a3d5128bedd44ca97b1f60
sh035 <saab35@naver.com>
2024-08-09 19:20:20 +0900

    chore: 불필요한 코드 주석 제거 (회원별 채팅방 조회 쿼리)

--- src/main/java/com/ddang/usedauction/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/ddang/usedauction/chat/repository/ChatRoomRepository.java
@@ -4,9 +4,6 @@ import com.ddang.usedauction.chat.domain.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-
-//  @Query("SELECT c FROM ChatRoom c WHERE c.seller.id = :memberId OR c.buyer.id = :memberId")
-//  List<ChatRoom> findChatRoomsByMemberId(@Param("memberId") Long memberId);
-
+  
   boolean existsByAuctionId(Long auctionId);
 }
