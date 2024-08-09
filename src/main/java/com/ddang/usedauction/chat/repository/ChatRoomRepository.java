8a44837d9658d8f4f600ccda426b60d5bcec6711
sh035 <saab35@naver.com>
2024-08-09 14:30:54 +0900

    comment: 회원별 채팅방 조회 쿼리 주석 처리

--- src/main/java/com/ddang/usedauction/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/ddang/usedauction/chat/repository/ChatRoomRepository.java
@@ -1,15 +1,12 @@
 package com.ddang.usedauction.chat.repository;
 
 import com.ddang.usedauction.chat.domain.entity.ChatRoom;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
-  @Query("SELECT c FROM ChatRoom c WHERE c.seller.id = :memberId OR c.buyer.id = :memberId")
-  List<ChatRoom> findChatRoomsByMemberId(@Param("memberId") Long memberId);
+//  @Query("SELECT c FROM ChatRoom c WHERE c.seller.id = :memberId OR c.buyer.id = :memberId")
+//  List<ChatRoom> findChatRoomsByMemberId(@Param("memberId") Long memberId);
 
   boolean existsByAuctionId(Long auctionId);
 }
