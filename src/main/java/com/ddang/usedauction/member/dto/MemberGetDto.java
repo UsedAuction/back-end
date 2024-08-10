52cb36c4a4ddbcc808f1074194528f1587db341c
jinhwan Bea <paewhal20@gmail.com>
2024-08-10 20:15:19 +0900

    feat: 회원 조회 시 dto

---
+++ src/main/java/com/ddang/usedauction/member/dto/MemberGetDto.java
@@ -0,0 +1,45 @@
+package com.ddang.usedauction.member.dto;
+
+import com.ddang.usedauction.member.domain.Member;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class MemberGetDto {
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    public static class Response {
+
+        private Long id;
+        private String memberId;
+        private String passWord;
+        private String email;
+        private boolean siteAlarm;
+        private long point;
+        private String social;
+        private String socialProviderId;
+        private LocalDateTime createdAt;
+
+        // entity -> getResponse
+        public static MemberGetDto.Response from(Member member) {
+
+            return Response.builder()
+                .id(member.getId())
+                .memberId(member.getMemberId())
+                .passWord(member.getPassWord())
+                .email(member.getEmail())
+                .siteAlarm(member.isSiteAlarm())
+                .point(member.getPoint())
+                .social(member.getSocial())
+                .socialProviderId(member.getSocialProviderId())
+                .createdAt(member.getCreatedAt())
+                .build();
+        }
+    }
+}
