67b687324eaa5d40c7854fb23bcf142ccbe85c39
sh035 <saab35@naver.com>
2024-08-11 20:50:24 +0900

    feat: 일반 사용자, OAuth2 사용자 정보 처리하는 클래스 구현

---
+++ src/main/java/com/ddang/usedauction/global/security/auth/PrincipalDetails.java
@@ -0,0 +1,84 @@
+package com.ddang.usedauction.global.security.auth;
+
+import com.ddang.usedauction.global.security.auth.userInfo.Oauth2UserInfo;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+  private Long id;
+  private String memberId;
+  private String password;
+  private String email;
+  private String role;
+  private Oauth2UserInfo oauth2UserInfo;
+
+  public PrincipalDetails(Long id, String memberId, String password, String email, String role) {
+    this.id = id;
+    this.memberId = memberId;
+    this.password = password;
+    this.email = email;
+    this.role = role;
+  }
+
+  public PrincipalDetails(Long id, String memberId,
+      String password, String email, String role, Oauth2UserInfo oauth2UserInfo) {
+    this.id = id;
+    this.memberId = memberId;
+    this.password = password;
+    this.email = email;
+    this.role = role;
+    this.oauth2UserInfo = oauth2UserInfo;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    Collection<GrantedAuthority> collection = new ArrayList<>();
+    collection.add((GrantedAuthority) () -> role);
+    return collection;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  @Override
+  public Map<String, Object> getAttributes() {
+    return oauth2UserInfo.getAttributes();
+  }
+
+  @Override
+  public String getName() {
+    return memberId;
+  }
+}
