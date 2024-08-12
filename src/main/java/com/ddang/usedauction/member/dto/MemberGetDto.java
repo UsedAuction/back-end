f86ede718b4021f7afa5038a16a473abe873af37
sh035 <saab35@naver.com>
2024-08-12 22:49:27 +0900

    refactor: 테스트 후 auction, category, member getDto에 Serializable 추가

--- src/main/java/com/ddang/usedauction/member/dto/MemberGetDto.java
+++ src/main/java/com/ddang/usedauction/member/dto/MemberGetDto.java
@@ -1,6 +1,7 @@
 package com.ddang.usedauction.member.dto;
 
 import com.ddang.usedauction.member.domain.Member;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -10,36 +11,36 @@ import lombok.NoArgsConstructor;
 
 public class MemberGetDto {
 
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @Builder(toBuilder = true)
-    public static class Response {
+  @Getter
+  @AllArgsConstructor
+  @NoArgsConstructor(access = AccessLevel.PROTECTED)
+  @Builder(toBuilder = true)
+  public static class Response implements Serializable {
 
-        private Long id;
-        private String memberId;
-        private String passWord;
-        private String email;
-        private boolean siteAlarm;
-        private long point;
-        private String social;
-        private String socialProviderId;
-        private LocalDateTime createdAt;
+    private Long id;
+    private String memberId;
+    private String passWord;
+    private String email;
+    private boolean siteAlarm;
+    private long point;
+    private String social;
+    private String socialProviderId;
+    private LocalDateTime createdAt;
 
-        // entity -> getResponse
-        public static MemberGetDto.Response from(Member member) {
+    // entity -> getResponse
+    public static MemberGetDto.Response from(Member member) {
 
-            return Response.builder()
-                .id(member.getId())
-                .memberId(member.getMemberId())
-                .passWord(member.getPassWord())
-                .email(member.getEmail())
-                .siteAlarm(member.isSiteAlarm())
-                .point(member.getPoint())
-                .social(member.getSocial())
-                .socialProviderId(member.getSocialProviderId())
-                .createdAt(member.getCreatedAt())
-                .build();
-        }
+      return Response.builder()
+          .id(member.getId())
+          .memberId(member.getMemberId())
+          .passWord(member.getPassWord())
+          .email(member.getEmail())
+          .siteAlarm(member.isSiteAlarm())
+          .point(member.getPoint())
+          .social(member.getSocial())
+          .socialProviderId(member.getSocialProviderId())
+          .createdAt(member.getCreatedAt())
+          .build();
     }
+  }
 }
