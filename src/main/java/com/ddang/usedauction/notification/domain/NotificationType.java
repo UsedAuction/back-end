103fd9681bbdfc2684bb64efc9f8ae1331bb7aa2
Cathunder <102372626+Cathunder@users.noreply.github.com>
2024-08-18 16:54:46 +0900

    feat: 즉시구매 알림 타입 추가

--- src/main/java/com/ddang/usedauction/notification/domain/NotificationType.java
+++ src/main/java/com/ddang/usedauction/notification/domain/NotificationType.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum NotificationType {
 
     DONE("DONE", "경매 종료"),
-    CHANGE_BID("CHANGE_BID", "최고 입찰가 알림"),
+    DONE_INSTANT("DONE_INSTANT", "즉시 구매로 인한 경매 종료"),
     QUESTION("QUESTION", "문의 질문 알림"),
     ANSWER("ANSWER", "문의 답변 알림"),
     CONFIRM("CONFIRM", "구매 확정 알림")
