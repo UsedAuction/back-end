862b40917e43fbaef094103e588e80334266c018
sh035 <saab35@naver.com>
2024-08-18 15:29:30 +0900

    refactor: 불필요한 필드 삭제, @RequiredArgsConstructor 추가

--- src/main/java/com/ddang/usedauction/token/service/RefreshTokenService.java
+++ src/main/java/com/ddang/usedauction/token/service/RefreshTokenService.java
@@ -1,62 +1,46 @@
 package com.ddang.usedauction.token.service;
 
-import com.ddang.usedauction.global.security.jwt.exception.CustomJwtException;
-import com.ddang.usedauction.global.security.jwt.exception.JwtErrorCode;
-import com.ddang.usedauction.member.domain.entity.Member;
-import com.ddang.usedauction.member.repository.MemberRepository;
-import com.ddang.usedauction.token.domain.entity.JwtToken;
-import jakarta.persistence.EntityNotFoundException;
+import com.ddang.usedauction.token.dto.TokenDto;
 import java.util.concurrent.TimeUnit;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class RefreshTokenService {
 
-  private final long refreshTokenExpiration;
-  private final MemberRepository memberRepository;
   private final RedisTemplate<String, Object> redisTemplate;
 
-  public RefreshTokenService(@Value("${spring.datasource.jwt.refresh.expiration}")
-  long refreshTokenExpiration,
-      MemberRepository memberRepository
-      , RedisTemplate<String, Object> redisTemplate) {
-    this.refreshTokenExpiration = refreshTokenExpiration;
-    this.memberRepository = memberRepository;
-    this.redisTemplate = redisTemplate;
-  }
-
   public void save(String email, String accessToken, String refreshToken) {
-    JwtToken token = JwtToken.builder()
+    TokenDto token = TokenDto.builder()
         .email(email)
         .accessToken(accessToken)
         .refreshToken(refreshToken)
         .build();
 
-    redisTemplate.opsForValue().set(refreshToken, token
-        , refreshTokenExpiration, TimeUnit.MILLISECONDS);
+    redisTemplate.opsForValue().set(email, token);
   }
 
-  public Member findMemberByRefreshToken(String refreshToken) {
-    JwtToken token = findTokenByRefreshToken(refreshToken);
-    if (token.getExpiration() > 0) {
-      return memberRepository.findByEmail(token.getEmail())
-          .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
-    }
-    throw new CustomJwtException(JwtErrorCode.INVALID_REFRESH_TOKEN);
+  public TokenDto findTokenByEmail(String email) {
+    return (TokenDto) redisTemplate.opsForValue().get(email);
   }
 
-  public JwtToken findTokenByRefreshToken(String refreshToken) {
-    JwtToken token = (JwtToken) redisTemplate.opsForValue().get(refreshToken);
-    if (token != null) {
-      return token;
-    }
-    throw new CustomJwtException(JwtErrorCode.INVALID_TOKEN);
+  public void updateToken(TokenDto token) {
+    redisTemplate.opsForValue().set(token.getEmail(), token);
   }
 
-  public void updateToken(JwtToken token) {
-    redisTemplate.opsForValue().set(token.getRefreshToken(), token,
-        token.getExpiration(), TimeUnit.MILLISECONDS);
+  public void deleteRefreshTokenByEmail(String email) {
+    redisTemplate.delete(email);
+  }
+
+  public void setBlackList(String key, Object o, Long milliSeconds) {
+    redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
+    redisTemplate.opsForValue().set(key, o, milliSeconds, TimeUnit.MILLISECONDS);
+  }
+
+  public boolean hasKeyBlackList(String key) {
+    return redisTemplate.hasKey(key);
   }
 }
