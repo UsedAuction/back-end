d4f5eec50148caad142a8a81b972e31ebee4f7a1
sh035 <saab35@naver.com>
2024-08-18 15:15:28 +0900

    refactor: token grantType 삭제, TTL 적용

--- src/main/java/com/ddang/usedauction/token/dto/TokenDto.java
+++ src/main/java/com/ddang/usedauction/token/dto/TokenDto.java
@@ -1,28 +1,25 @@
-package com.ddang.usedauction.token.domain.entity;
+package com.ddang.usedauction.token.dto;
 
 import jakarta.persistence.Id;
-import java.util.concurrent.TimeUnit;
+import java.io.Serializable;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.RedisHash;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class JwtToken {
+@RedisHash(timeToLive = 60 * 60 * 24 * 7)
+public class TokenDto implements Serializable {
 
   @Id
   private String email;
-  private String grantType;
-  private String accessToken;
   private String refreshToken;
-
-  @TimeToLive(unit = TimeUnit.MILLISECONDS)
-  private long expiration;
+  private String accessToken;
 
   public void setAccessToken(String accessToken) {
     this.accessToken = accessToken;
