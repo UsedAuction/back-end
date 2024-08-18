6b7eb812fa60033475138f78f413329680b7fa5a
sh035 <saab35@naver.com>
2024-08-18 15:24:11 +0900

    add: AuthenticationFilter에 refreshTokenService 의존성 주입

--- src/main/java/com/ddang/usedauction/config/SecurityConfig.java
+++ src/main/java/com/ddang/usedauction/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.ddang.usedauction.config;
 
-import com.ddang.usedauction.global.security.auth.PrincipalOauth2UserService;
-import com.ddang.usedauction.global.security.jwt.JwtAuthenticationFilter;
-import com.ddang.usedauction.global.security.jwt.Oauth2FailureHandler;
-import com.ddang.usedauction.global.security.jwt.Oauth2SuccessHandler;
-import com.ddang.usedauction.global.security.jwt.TokenProvider;
+import com.ddang.usedauction.security.auth.PrincipalOauth2UserService;
+import com.ddang.usedauction.security.jwt.JwtAuthenticationFilter;
+import com.ddang.usedauction.security.jwt.Oauth2FailureHandler;
+import com.ddang.usedauction.security.jwt.Oauth2SuccessHandler;
+import com.ddang.usedauction.security.jwt.TokenProvider;
+import com.ddang.usedauction.token.service.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,9 +21,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class securityConfig {
+public class SecurityConfig {
 
   private final TokenProvider tokenProvider;
+  private final RefreshTokenService refreshTokenService;
   private final PrincipalOauth2UserService principalOauth2UserService;
   private final Oauth2SuccessHandler oauth2SuccessHandler;
   private final Oauth2FailureHandler oauth2FailureHandler;
@@ -41,14 +43,15 @@ public class securityConfig {
         .authorizeHttpRequests(auth -> auth
             .anyRequest().permitAll()
         )
-        .oauth2Login(oauth -> // OAuth2 로그인 기능에 대한 여러 설정의 진입점
+        .oauth2Login(oauth -> oauth// OAuth2 로그인 기능에 대한 여러 설정의 진입점
+            // 로그인 성공 시 핸들러
+            .successHandler(oauth2SuccessHandler)
+            // 로그인 실패 시 핸들러
+            .failureHandler(oauth2FailureHandler)
             // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정을 담당
-            oauth.userInfoEndpoint(userInfo -> userInfo.userService(principalOauth2UserService))
-                // 로그인 성공 시 핸들러
-                .successHandler(oauth2SuccessHandler)
-                .failureHandler(oauth2FailureHandler)
+            .userInfoEndpoint(userInfo -> userInfo.userService(principalOauth2UserService))
         )
-        .addFilterBefore(new JwtAuthenticationFilter(tokenProvider),
+        .addFilterBefore(new JwtAuthenticationFilter(tokenProvider, refreshTokenService),
             UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
