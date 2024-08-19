46655de3e785d9fac4fbd4afab323b8df4307814
jinhwanB <paewhal20@gmail.com>
2024-08-19 21:12:48 +0900

    feat: 회원이 입찰에 참여한 경매 조회 메소드

--- src/main/java/com/ddang/usedauction/auction/repository/AuctionRepository.java
+++ src/main/java/com/ddang/usedauction/auction/repository/AuctionRepository.java
@@ -1,10 +1,15 @@
 package com.ddang.usedauction.auction.repository;
 
 import com.ddang.usedauction.auction.domain.Auction;
+import com.ddang.usedauction.auction.domain.AuctionState;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long>, AuctionRepositoryCustom {
 
+    @Query("select a from Auction a left join a.bidList b where b.member.memberId = :memberId and a.auctionState = :auctionState")
+    List<Auction> findAllByMemberIdAndAuctionState(String memberId, AuctionState auctionState);
 }
