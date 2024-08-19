7eb707f39508b69107a1c9a83c34705813da1eb2
sh035 <saab35@naver.com>
2024-08-19 11:33:39 +0900

    refactor: 사용하지 않는 에러코드 삭제

--- src/main/java/com/ddang/usedauction/security/jwt/exception/JwtErrorCode.java
+++ src/main/java/com/ddang/usedauction/security/jwt/exception/JwtErrorCode.java
@@ -7,11 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum JwtErrorCode {
   INVALID_TOKEN("잘못된 토큰입니다."),
-  INVALID_REFRESH_TOKEN("잘못된 리프레쉬 토큰입니다."),
   EXPIRED_TOKEN("만료된 토큰입니다."),
   EXPIRED_REFRESH_TOKEN("만료된 리프레쉬 토큰입니다."),
-  UNSUPPORTED_TOKEN("지원되지 않는 토큰입니다."),
-  EMPTY_CLAIM("JWT 클레임 문자열이 비어 있습니다.");
+  UNSUPPORTED_TOKEN("지원되지 않는 토큰입니다.");
 
   private final String message;
 }
