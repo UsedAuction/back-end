a821925a911b8231693aab7a0c82a23c7eda377e
sh035 <saab35@naver.com>
2024-08-20 14:53:39 +0900

    add: Role @Enumerated 추가

--- src/main/java/com/ddang/usedauction/member/domain/Member.java
+++ src/main/java/com/ddang/usedauction/member/domain/Member.java
@@ -4,6 +4,8 @@ import com.ddang.usedauction.config.BaseTimeEntity;
 import com.ddang.usedauction.member.domain.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,6 +39,7 @@ public class Member extends BaseTimeEntity {
   private String email;
 
   @Column
+  @Enumerated(EnumType.STRING)
   private Role role;
 
   @Column
