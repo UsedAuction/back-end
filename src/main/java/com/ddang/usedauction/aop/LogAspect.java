481892247bf7364e83c7e2753ebfef35426eea64
sh035 <saab35@naver.com>
2024-08-21 20:34:16 +0900

    add: AOP 포인트컷 security 제외 추가

--- src/main/java/com/ddang/usedauction/aop/LogAspect.java
+++ src/main/java/com/ddang/usedauction/aop/LogAspect.java
@@ -15,48 +15,49 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspect {
 
-    // 모든 메소드 포인트컷
-    @Pointcut("execution(* com.ddang.usedauction..*(..)) "
-        + "&& !execution(* com.ddang.usedauction.config..*(..)) "
-        + "&& !execution(* com.ddang.usedauction.validation..*(..))"
-        + "&& !execution(* com.ddang.usedauction.util..*(..))")
-    public void all() {
-    }
+  // 모든 메소드 포인트컷
+  @Pointcut("execution(* com.ddang.usedauction..*(..)) "
+      + "&& !execution(* com.ddang.usedauction.config..*(..)) "
+      + "&& !execution(* com.ddang.usedauction.validation..*(..))"
+      + "&& !execution(* com.ddang.usedauction.util..*(..))"
+      + "&& !execution(* com.ddang.usedauction.security..*(..))")
+  public void all() {
+  }
 
-    // Controller클래스의 모든 메소드 포인트컷
-    @Pointcut("execution(* com.ddang.usedauction..*Controller.*(..))")
-    public void controller() {
-    }
+  // Controller클래스의 모든 메소드 포인트컷
+  @Pointcut("execution(* com.ddang.usedauction..*Controller.*(..))")
+  public void controller() {
+  }
 
-    // Service클래스의 모든 메소드 포인트컷
-    @Pointcut("execution(* com.ddang.usedauction..*Service.*(..))")
-    public void service() {
-    }
+  // Service클래스의 모든 메소드 포인트컷
+  @Pointcut("execution(* com.ddang.usedauction..*Service.*(..))")
+  public void service() {
+  }
 
-    // Service 또는 Controller의 메소드 실행 전 로그 표시
-    @Before("controller() || service()")
-    public void beforeLog(JoinPoint joinPoint) {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method method = signature.getMethod();
-        String className = joinPoint.getTarget().getClass().getSimpleName();
-        log.debug("[{}] {}메소드 실행", className, method.getName());
+  // Service 또는 Controller의 메소드 실행 전 로그 표시
+  @Before("controller() || service()")
+  public void beforeLog(JoinPoint joinPoint) {
+    MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+    Method method = signature.getMethod();
+    String className = joinPoint.getTarget().getClass().getSimpleName();
+    log.debug("[{}] {}메소드 실행", className, method.getName());
 
-        Object[] args = joinPoint.getArgs();
-        for (Object arg : args) {
-            if (arg != null) {
-                log.debug("type = {}", arg.getClass().getSimpleName());
-                log.debug("value = {}", arg);
-            }
-        }
+    Object[] args = joinPoint.getArgs();
+    for (Object arg : args) {
+      if (arg != null) {
+        log.debug("type = {}", arg.getClass().getSimpleName());
+        log.debug("value = {}", arg);
+      }
     }
+  }
 
-    // 모든 메소드에서 예외 발생시 로그 표시
-    @AfterThrowing(pointcut = "all()", throwing = "ex")
-    public void afterThrowingLog(JoinPoint joinPoint, Throwable ex) {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Method method = methodSignature.getMethod();
-        String className = joinPoint.getTarget().getClass().getSimpleName();
-        log.error("[{}] {}메소드에서 예외 발생", className, method.getName());
-        log.error("Exception message = {}", ex.getMessage());
-    }
+  // 모든 메소드에서 예외 발생시 로그 표시
+  @AfterThrowing(pointcut = "all()", throwing = "ex")
+  public void afterThrowingLog(JoinPoint joinPoint, Throwable ex) {
+    MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+    Method method = methodSignature.getMethod();
+    String className = joinPoint.getTarget().getClass().getSimpleName();
+    log.error("[{}] {}메소드에서 예외 발생", className, method.getName());
+    log.error("Exception message = {}", ex.getMessage());
+  }
 }
