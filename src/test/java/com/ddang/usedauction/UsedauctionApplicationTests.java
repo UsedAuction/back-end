09ed0f3e5cbd1a2d3ea331db86c7db75cbc7100d
jinhwanB <paewhal20@gmail.com>
2024-08-22 01:09:30 +0900

    테스트 커밋

--- src/test/java/com/ddang/usedauction/UsedauctionApplicationTests.java
+++ src/test/java/com/ddang/usedauction/UsedauctionApplicationTests.java
@@ -1,13 +1,15 @@
 package com.ddang.usedauction;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("Temporarily disabling this test")
 class UsedauctionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
