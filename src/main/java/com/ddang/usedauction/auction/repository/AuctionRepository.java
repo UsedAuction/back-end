2eef05190971f53eb8c8ca1c3589e20b1b6cfdf6
jinhwanB <paewhal20@gmail.com>
2024-08-22 11:15:02 +0900

    feat:left join을 inner join으로 변경
    left join 시 참여하지 않은 경우도 조회되어 inner join으로 변경

--- src/main/java/com/ddang/usedauction/auction/repository/AuctionRepository.java
+++ src/main/java/com/ddang/usedauction/auction/repository/AuctionRepository.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long>, AuctionRepositoryCustom {
 
-    @Query("select a from Auction a left join a.bidList b where b.member.memberId = :memberId and a.auctionState = :auctionState")
-    List<Auction> findAllByMemberIdAndAuctionState(String memberId, AuctionState auctionState);
+    @Query("select a from Auction a inner join a.bidList b where b.member.memberId = :memberId and a.auctionState = :auctionState")
+    List<Auction> findAllByMemberIdAndAuctionState(String memberId,
+        AuctionState auctionState); // 현재 회원이 참여중인 경매 리스트 조회
 }
