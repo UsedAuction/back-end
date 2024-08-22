63b16f9a489df1675817157c18f37c4b46a0a010
jinhwanB <paewhal20@gmail.com>
2024-08-23 03:20:20 +0900

    feat: 문의 단건 조회

---
+++ src/main/java/com/ddang/usedauction/ask/service/AskService.java
@@ -0,0 +1,40 @@
+package com.ddang.usedauction.ask.service;
+
+import com.ddang.usedauction.ask.domain.Ask;
+import com.ddang.usedauction.ask.dto.AskCreateDto;
+import com.ddang.usedauction.ask.dto.AskUpdateDto;
+import com.ddang.usedauction.ask.repository.AskRepository;
+import com.ddang.usedauction.auction.domain.Auction;
+import com.ddang.usedauction.auction.domain.AuctionState;
+import com.ddang.usedauction.auction.repository.AuctionRepository;
+import com.ddang.usedauction.member.domain.Member;
+import com.ddang.usedauction.member.repository.MemberRepository;
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AskService {
+
+    private final AskRepository askRepository;
+    private final AuctionRepository auctionRepository;
+    private final MemberRepository memberRepository;
+
+    /**
+     * 문의 단건 조회
+     *
+     * @param askId 문의 pk
+     * @return 조회된 문의
+     */
+    @Transactional(readOnly = true)
+    public Ask getAsk(Long askId) {
+
+        return askRepository.findById(askId)
+            .orElseThrow(() -> new NoSuchElementException("존재하지 않는 문의입니다."));
+    }
+
