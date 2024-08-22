6edf5209b6403722704ee870efe6776040595d01
sh035 <saab35@naver.com>
2024-08-23 01:17:12 +0900

    refactor: entity -> dto 변환없이 dto 반환

--- src/main/java/com/ddang/usedauction/category/controller/CategoryController.java
+++ src/main/java/com/ddang/usedauction/category/controller/CategoryController.java
@@ -4,9 +4,7 @@ import com.ddang.usedauction.category.dto.CategoryByParentDto;
 import com.ddang.usedauction.category.dto.ParentCategoryDto;
 import com.ddang.usedauction.category.service.CategoryService;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,11 +22,8 @@ public class CategoryController {
    * 대분류 카테고리 클릭 시 대분류에 포함된 모든 상품 조회
    */
   @GetMapping
-  public ResponseEntity<List<ParentCategoryDto>> getAllParentCategories() {
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(categoryService.getAllParentCategories().stream()
-            .map(ParentCategoryDto::from)
-            .collect(Collectors.toList()));
+  public ResponseEntity<List<ParentCategoryDto>> findAllParentCategories() {
+    return ResponseEntity.ok(categoryService.findAllParentCategories());
   }
 
   /**
@@ -37,8 +32,8 @@ public class CategoryController {
    * @param parentId 부모 카테고리 Id
    */
   @GetMapping("/{parentId}")
-  public ResponseEntity<CategoryByParentDto> getChildrenByParentId(
+  public ResponseEntity<CategoryByParentDto> findChildrenByParentId(
       @PathVariable("parentId") Long parentId) {
-    return ResponseEntity.ok(categoryService.getCategoriesByParentId(parentId));
+    return ResponseEntity.ok(categoryService.findCategoriesByParentId(parentId));
   }
 }
