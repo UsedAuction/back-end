7fbcbdd6c0a06ed9a24e45221a710ce9f826d084
jinhwanB <paewhal20@gmail.com>
2024-08-23 18:44:56 +0900

    add: 유효성 검증 어노테이션 추가

--- src/main/java/com/ddang/usedauction/ask/dto/AskCreateDto.java
+++ src/main/java/com/ddang/usedauction/ask/dto/AskCreateDto.java
@@ -1,5 +1,8 @@
 package com.ddang.usedauction.ask.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,7 +15,13 @@ import lombok.NoArgsConstructor;
 @Builder(toBuilder = true)
 public class AskCreateDto { // 문의글 생성 시 dto
 
+    @NotBlank(message = "제목을 입력해주세요.")
     private String title; // 제목
+
+    @NotBlank(message = "내용을 입력해주세요.")
     private String content; // 내용
+
+    @NotNull(message = "pk 값은 null 일 수 없습니다.")
+    @Positive(message = "pk 값은 0 또는 음수일 수 없습니다.")
     private Long auctionId; // 경매 pk
 }
--- src/main/java/com/ddang/usedauction/ask/dto/AskUpdateDto.java
+++ src/main/java/com/ddang/usedauction/ask/dto/AskUpdateDto.java
@@ -1,5 +1,6 @@
 package com.ddang.usedauction.ask.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,5 +13,6 @@ import lombok.NoArgsConstructor;
 @Builder(toBuilder = true)
 public class AskUpdateDto { // 문의 수정 시 dto
 
+    @NotBlank(message = "내용을 입력해주세요.")
     private String content; // 내용
 }
