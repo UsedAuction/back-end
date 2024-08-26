bd9b110efca872c2cf9cc4a914c7f027acecf974
sh035 <saab35@naver.com>
2024-08-26 15:53:21 +0900

    test: 카테고리 서비스 테스트 수정

--- src/test/java/com/ddang/usedauction/category/service/CategoryServiceTest.java
+++ src/test/java/com/ddang/usedauction/category/service/CategoryServiceTest.java
@@ -1,127 +1,108 @@
 package com.ddang.usedauction.category.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
-
 import com.ddang.usedauction.category.domain.Category;
-import com.ddang.usedauction.category.dto.CategoryByParentDto;
-import com.ddang.usedauction.category.dto.ChildCategoryDto;
+import com.ddang.usedauction.category.dto.CategoryDto;
 import com.ddang.usedauction.category.repository.CategoryRepository;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.cache.CacheManager;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
 
-  @Mock
-  CategoryRepository categoryRepository;
+    @Mock
+    CategoryRepository categoryRepository;
 
-  @InjectMocks
-  CategoryService categoryService;
+    @Mock
+    CacheManager cacheManager;
 
-  Category parentCategory1;
-  Category parentCategory2;
-  List<Category> childCategories1;
-  List<Category> childCategories2;
+    @InjectMocks
+    CategoryService categoryService;
+
+    Category parentCategory1;
+    Category parentCategory2;
+    List<Category> childCategories1;
+    List<Category> childCategories2;
 
 
-  @BeforeEach
-  void setUp() {
-    parentCategory1 = Category.builder()
-        .id(1L)
-        .categoryName("parent Category1")
-        .parentId(null)
-        .build();
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
 
-    Category child1 = Category.builder()
-        .id(3L)
-        .categoryName("child Category1-1")
-        .parentId(1L)
-        .build();
+        parentCategory1 = Category.builder()
+            .id(1L)
+            .categoryName("parent Category1")
+            .parentId(null)
+            .build();
 
-    Category child2 = Category.builder()
-        .id(4L)
-        .categoryName("child Category1-2")
-        .parentId(1L)
-        .build();
+        Category child1 = Category.builder()
+            .id(3L)
+            .categoryName("child Category1-1")
+            .parentId(1L)
+            .build();
 
-    parentCategory2 = Category.builder()
-        .id(2L)
-        .categoryName("parent Category2")
-        .parentId(null)
-        .build();
+        Category child2 = Category.builder()
+            .id(4L)
+            .categoryName("child Category1-2")
+            .parentId(1L)
+            .build();
 
-    Category child3 = Category.builder()
-        .id(5L)
-        .categoryName("child Category2-1")
-        .parentId(2L)
-        .build();
+        parentCategory2 = Category.builder()
+            .id(2L)
+            .categoryName("parent Category2")
+            .parentId(null)
+            .build();
 
-    Category child4 = Category.builder()
-        .id(6L)
-        .categoryName("child Category2-2")
-        .parentId(21L)
-        .build();
+        Category child3 = Category.builder()
+            .id(5L)
+            .categoryName("child Category2-1")
+            .parentId(2L)
+            .build();
 
-    childCategories1 = new ArrayList<>();
-    childCategories1.add(child1);
-    childCategories1.add(child2);
+        Category child4 = Category.builder()
+            .id(6L)
+            .categoryName("child Category2-2")
+            .parentId(21L)
+            .build();
 
-    childCategories2 = new ArrayList<>();
-    childCategories2.add(child3);
-    childCategories2.add(child4);
-  }
+        childCategories1 = new ArrayList<>();
+        childCategories1.add(child1);
+        childCategories1.add(child2);
 
-  @Test
-  @DisplayName("부모 카테고리들 찾기")
-  void findParentCategories() {
-    List<Category> parentCategories = new ArrayList<>();
-    parentCategories.add(parentCategory1);
-    parentCategories.add(parentCategory2);
+        childCategories2 = new ArrayList<>();
+        childCategories2.add(child3);
+        childCategories2.add(child4);
 
-    when(categoryRepository.findAllParentCategories()).thenReturn(parentCategories);
+    }
 
-    List<Category> result = categoryRepository.findAllParentCategories();
+    @Test
+    void testGetAllCategoriesExecutionTime() {
+        // 실행 시간 측정 시작
+        long startTime = System.nanoTime();
 
-    assertThat(result).isNotNull();
-    assertThat(result.size()).isEqualTo(2);
-    assertThat(result.get(0).getCategoryName()).isEqualTo("parent Category1");
-    assertThat(result.get(1).getCategoryName()).isEqualTo("parent Category2");
-  }
+        List<CategoryDto> categories = categoryService.getAllCategories();
+        // 실행 시간 측정 종료
+        long endTime = System.nanoTime();
+        long executionTime = (endTime - startTime) / 1_000_000;  // 밀리초로 변환
 
-  @Test
-  @DisplayName("특정 부모 카테고리와 해당하는 자식 카테고리들 찾기")
-  void findCategoriesByParentId() {
-    when(categoryRepository.findById(1L)).thenReturn(Optional.of(parentCategory1));
-    when(categoryRepository.findCategoriesByParentId(1L)).thenReturn(childCategories1);
+        System.out.println("카테고리 조회 시간 " + executionTime + " ms");
 
-    CategoryByParentDto result = categoryService.findCategoriesByParentId(1L);
+        long startTime2 = System.nanoTime();
 
-    assertThat(result).isNotNull();
-    assertThat(result.getId()).isEqualTo(1L);
-    assertThat(result.getCategoryName()).isEqualTo("parent Category1");
-    assertThat(result.getCategories()).hasSize(2);
+        List<CategoryDto> categories2 = categoryService.getAllCategories();
 
-    List<ChildCategoryDto> children = result.getCategories();
-    assertThat(children.get(0).getCategoryName()).isEqualTo("child Category1-1");
-    assertThat(children.get(1).getCategoryName()).isEqualTo("child Category1-2");
-  }
+        long endTime2 = System.nanoTime();
+        long executionTime2 = (endTime2 - startTime2) / 1_000_000;  // 밀리초로 변환
 
-  @Test
-  @DisplayName("특정 부모 카테고리와 해당하는 자식 카테고리들 찾기 - 실패 (부모카테고리 존재x)")
-  void findCategoriesByParentId_NOTExist() {
-    when(categoryRepository.findById(1L)).thenReturn(Optional.empty());
+        System.out.println("카테고리 조회 시간 " + executionTime2 + " ms");
 
-    assertThrows(NoSuchElementException.class, () -> categoryService.findCategoriesByParentId(1L));
-  }
+    }
+
 }
