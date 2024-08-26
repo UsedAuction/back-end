a43d1056dccbcb7e5cb1ce6908705cf94f8b812a
jinhwanB <paewhal20@gmail.com>
2024-08-26 17:35:45 +0900

    feat: allowedOrigins 와일드카드에서 프론트 도메인으로 변경

--- src/main/java/com/ddang/usedauction/config/WebConfig.java
+++ src/main/java/com/ddang/usedauction/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins("https://dddang.vercel.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Authorization", "Content-Type")
             .allowCredentials(true)
