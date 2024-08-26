e79844a4060d3dcbff78fe8e05fea733c63dd2f7
sh035 <saab35@naver.com>
2024-08-26 23:22:55 +0900

    chore: 컨트롤러 주석 추가

--- src/main/java/com/ddang/usedauction/category/controller/CategoryController.java
+++ src/main/java/com/ddang/usedauction/category/controller/CategoryController.java
@@ -16,6 +16,9 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    /**
+     * 전체 카테고리 리스트 조회 부모 카테고리에 딸린 자식 카테고리들도 조회
+     */
     @GetMapping
     public ResponseEntity<List<CategoryDto>> getAllCategories() {
         return ResponseEntity.ok(categoryService.getAllCategories());
