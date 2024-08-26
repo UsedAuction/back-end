e3e623477cc97516b148f0a52f5861ffbd70a74b
sh035 <saab35@naver.com>
2024-08-26 23:47:45 +0900

    refactor: @Primary 제거

--- src/main/java/com/ddang/usedauction/config/RedisConfig.java
+++ src/main/java/com/ddang/usedauction/config/RedisConfig.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -34,7 +33,6 @@ public class RedisConfig {
     private String host;
 
     @Bean
-    @Primary
     public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
 
         RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
