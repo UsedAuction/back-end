037ec17d3c35167667c72a2316fbe55c92b92c9c
jinhwanB <paewhal20@gmail.com>
2024-08-27 19:51:21 +0900

    feat: 개발 도메인 주소로 리다이렉트 주소 변경

--- src/main/java/com/ddang/usedauction/security/jwt/Oauth2SuccessHandler.java
+++ src/main/java/com/ddang/usedauction/security/jwt/Oauth2SuccessHandler.java
@@ -22,7 +22,7 @@ public class Oauth2SuccessHandler implements AuthenticationSuccessHandler {
 
     @Value("${spring.jwt.refresh.expiration}")
     private int refreshTokenExpirationValue;
-    private static final String URI = "localhost:5173";
+    private static final String URI = "https://localhost:5173";
     private final TokenProvider tokenProvider;
     private final RefreshTokenService refreshTokenService;
 
