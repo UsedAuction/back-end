fb9439aa6735427748b01d4dd4e4605be10ef0c7
jinhwanB <paewhal20@gmail.com>
2024-08-27 17:04:05 +0900

    feat: 회원 pk 사용에서 회원 아이디 사용으로 변경

--- src/main/java/com/ddang/usedauction/chat/domain/dto/ChatMessageSendDto.java
+++ src/main/java/com/ddang/usedauction/chat/domain/dto/ChatMessageSendDto.java
@@ -10,36 +10,36 @@ import lombok.NoArgsConstructor;
 
 public class ChatMessageSendDto {
 
-  @Getter
-  @AllArgsConstructor
-  @NoArgsConstructor(access = AccessLevel.PROTECTED)
-  @Builder
-  public static class Request {
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder
+    public static class Request {
 
-    private Long roomId;
-    private Long senderId;
-    private String message;
+        private Long roomId;
+        private String senderId;
+        private String message;
 
-  }
-
-  @Getter
-  @AllArgsConstructor
-  @NoArgsConstructor(access = AccessLevel.PROTECTED)
-  @Builder
-  public static class Response {
-
-    private Long roomId;
-    private Member sender;
-    private String message;
-
-    public static Response from(ChatMessage chatMessage) {
-      return Response.builder()
-          .roomId(chatMessage.getChatRoom().getId())
-          .sender(chatMessage.getSender())
-          .message(chatMessage.getMessage())
-          .build();
     }
-  }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder
+    public static class Response {
+
+        private Long roomId;
+        private Member sender;
+        private String message;
+
+        public static Response from(ChatMessage chatMessage) {
+            return Response.builder()
+                .roomId(chatMessage.getChatRoom().getId())
+                .sender(chatMessage.getSender())
+                .message(chatMessage.getMessage())
+                .build();
+        }
+    }
 
 
 }
