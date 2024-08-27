6727ee25bca343311905c7e63bd27e81db6a5170
sh035 <saab35@naver.com>
2024-08-28 03:19:02 +0900

    feat: 회원가입 DTO 구현

---
+++ src/main/java/com/ddang/usedauction/member/dto/MemberSignUpDto.java
@@ -0,0 +1,44 @@
+package com.ddang.usedauction.member.dto;
+
+import com.ddang.usedauction.member.domain.Member;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(toBuilder = true)
+public class MemberSignUpDto {
+
+    @NotBlank(message = "아이디는 필수 입력값입니다.")
+    @Pattern(regexp = "^[a-z0-9]{6,12}$", message = "아이디는 영문 소문자와 숫자 6~12자리여야 합니다.")
+    private String memberId;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z].*)(?=.*[0-9!@#$%^&*()\\-_=+{};:,<.>].*).{4,16}$",
+        message = "비밀번호는 4자에서 16자 사이로 영문자, 숫자, 특수문자 중 2가지 이상을 조합해야 합니다.")
+    private String password;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z].*)(?=.*[0-9!@#$%^&*()\\-_=+{};:,<.>].*).{4,16}$",
+        message = "비밀번호는 4자에서 16자 사이로 영문자, 숫자, 특수문자 중 2가지 이상을 조합해야 합니다.")
+    private String confirmPassword;
+
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+        message = "유효한 이메일 주소를 입력해주세요.")
+    private String email;
+
+    public static MemberSignUpDto from(Member member) {
+        return MemberSignUpDto.builder()
+            .memberId(member.getMemberId())
+            .password(member.getPassWord())
+            .email(member.getEmail())
+            .build();
+    }
+}
