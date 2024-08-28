30c586c2ea58e77f8688cae88f8d18340affafbf
seungh22 <114427072+seungh22@users.noreply.github.com>
2024-08-29 02:48:52 +0900

    feat: 사용하지 않는 어노테이션 삭제

--- src/main/java/com/ddang/usedauction/member/domain/Member.java
+++ src/main/java/com/ddang/usedauction/member/domain/Member.java
@@ -1,7 +1,7 @@
 package com.ddang.usedauction.member.domain;
 
 import com.ddang.usedauction.config.BaseTimeEntity;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ddang.usedauction.member.dto.MemberServiceDto;
 import com.ddang.usedauction.member.domain.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,10 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,67 +25,25 @@ import org.hibernate.annotations.SQLRestriction;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder(toBuilder = true)
 @SQLRestriction("deleted_at IS NULL")
+
 public class Member extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "아이디를 입력해주세요.")
-    @Size(min = 6, max = 12)
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디는 영문자와 숫자만 사용 가능합니다.")
-    @Column(nullable = false, length = 20, unique = true)
+    @Column(nullable = false, length = 12, unique = true)
     private String memberId;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Size(min = 4, max = 16)
-    @Pattern(
-            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).{4,16}$",
-            message = "비밀번호는 4~16자 이내의 숫자, 특수문자, 영문자(대소문자) 중 2가지 이상을 포함해야 합니다."
-    )
-    @JsonProperty
     @Column(nullable = false)
     private String passWord;
 
-    @Email(message = "유효한 이메일 주소를 입력해주세요.")
-    @NotBlank(message = "이메일을 입력해주세요.")
     @Column(nullable = false, unique = true)
     private String email;
-    private LocalDateTime updateDate;
 
-    public String getMemberId() {
-        return memberId;
-    }
-
-    public String getPassWord() {
-        return passWord;
-    }
-
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setMemberId(String memberId) {
-        this.memberId = memberId;
-    }
-
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void update(Member newMember) {
-        memberId = newMember.memberId;
-        passWord = newMember.passWord;
-        email = newMember.email;
-    }
-    public boolean isValidPassWord(String password) {
-        return this.passWord.equals(password);
-    }
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Column
     private boolean siteAlarm;
@@ -100,11 +54,27 @@ public class Member extends BaseTimeEntity {
     @Column
     private String social;
 
-  @Column
-  private String socialProviderId;
-
     @Column
-    private LocalDateTime deleteDate;
+    private String socialProviderId;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public void updateMemberId(String newMemberId) {
+        this.memberId = newMemberId;
+    }
+
+    public void updateEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+    public void updatePassword(String newPassword) {
+        this.passWord = newPassword;
+    }
+
+    public boolean isValidPassWord(String password) {
+        return this.passWord.equals(password);
+    }
 
     /**
      * Entity -> ServiceDto
@@ -112,19 +82,17 @@ public class Member extends BaseTimeEntity {
      * @return ServiceDto
      */
     public MemberServiceDto toServiceDto() {
-
         return MemberServiceDto.builder()
                 .id(id)
                 .memberId(memberId)
                 .passWord(passWord)
                 .email(email)
+                .role(role)
                 .createDate(getCreatedAt())
                 .updateDate(getUpdatedAt())
-                .deleteDate(getDeletedAt())
+                .deleteDate(deletedAt)
                 .build();
     }
-  @Column
-  private LocalDateTime deletedAt;
 
     // 포인트 충전
     public void addPoint(int point) {
