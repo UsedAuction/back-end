8eef740a454834f8cb00cc679fa7555f51246d05
seungh22 <114427072+seungh22@users.noreply.github.com>
2024-08-29 03:31:50 +0900

    add: MemberUpdateDto내의 회원정보 수정 추가

--- src/main/java/com/ddang/usedauction/member/dto/MemberUpdateDto.java
+++ src/main/java/com/ddang/usedauction/member/dto/MemberUpdateDto.java
@@ -8,6 +8,7 @@ import lombok.*;
 
 public class MemberUpdateDto {
 
+
     /**
      * 회원정보 수정 Dto
      */
@@ -18,18 +19,26 @@ public class MemberUpdateDto {
     @Builder(toBuilder = true)
     public static class Request{
 
-        @NotBlank(message = "아이디를 입력해주세요.")
+        @NotBlank(message = "새로운 아이디를 입력해주세요.")
         @Size(min = 6, max = 12, message = "아이디는 6~12자 사이로 입력해주세요.")
         @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디는 영문자와 숫자만 사용 가능합니다.")
         private String memberId;
 
-        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
         @Size(min = 4, max = 16, message = "비밀번호는 4~16자 사이로 입력해주세요.")
         @Pattern(
                 regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).{8,16}$",
                 message = "비밀번호는 4~16자 이내의 숫자, 특수문자, 영문자(대소문자) 중 2가지 이상을 포함해야 합니다."
         )
         private String passWord; // 수정할 비밀번호
+
+        @NotBlank(message = "새로운 비밀번호를 다시 한번 더 입력해주세요.")
+        @Size(min = 4, max = 16, message = "비밀번호는 4~16자 사이로 입력해주세요.")
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).{8,16}$",
+                message = "비밀번호는 4~16자 이내의 숫자, 특수문자, 영문자(대소문자) 중 2가지 이상을 포함해야 합니다."
+        )
+        private String confirmNewPassword;
 
         @Email(message = "유효한 이메일 주소를 입력해주세요.")
         @NotBlank(message = "이메일을 입력해주세요.")
@@ -40,9 +49,9 @@ public class MemberUpdateDto {
     @AllArgsConstructor
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @Builder(toBuilder = true)
-    public static class Response{
+    public static class Response{ // 바뀐정보로 넘기는지 확인하기
 
-        private String memberId; // 아이디
+        private String memberId; // 수정된 아이디
         private String passWord; // 수정된 비밀번호
         private String email; // 수정된 이메일
     }
