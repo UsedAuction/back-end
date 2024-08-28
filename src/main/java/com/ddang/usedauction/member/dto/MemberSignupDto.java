2cacd02f5ecb68bc21832fa1c46e6cf318928f71
seungh22 <114427072+seungh22@users.noreply.github.com>
2024-08-29 03:29:10 +0900

    add: MemberSignupDto 추가

--- src/main/java/com/ddang/usedauction/member/dto/MemberSignupDto.java
+++ src/main/java/com/ddang/usedauction/member/dto/MemberSignupDto.java
@@ -1,5 +1,7 @@
 package com.ddang.usedauction.member.dto;
 
+import com.ddang.usedauction.member.domain.enums.Role;
+import com.ddang.usedauction.validation.IsEnum;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -35,8 +37,23 @@ public class MemberSignupDto {
         )
         private String passWord;
 
-        @Email(message = "유효한 이메일 주소를 입력해주세요.")
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Size(min = 4, max = 16, message = "비밀번호는 4~16자 사이로 입력해주세요.")
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).{8,16}$",
+                message = "비밀번호는 4~16자 이내의 숫자, 특수문자, 영문자(대소문자) 중 2가지 이상을 포함해야 합니다."
+        )
+        private String confirmPassWord;
+
         @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "유효한 이메일 주소를 입력해주세요.")
+        @Pattern(regexp = "[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$",
+                message = "유효한 이메일 주소를 입력해주세요.")
+
         private String email;
+
+//        @NotBlank(message = "인증코드를 입력해주세요.")
+//        @Pattern(regexp = "\\d{4}", message = "인증코드는 4자리 숫자입니다.")
+//        private String AuthCode; // 인증코드번호
     }
 }
