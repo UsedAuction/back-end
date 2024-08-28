8ea79c91cb582c3688d062b2814388de8c90e14e
seungh22 <114427072+seungh22@users.noreply.github.com>
2024-08-29 03:25:11 +0900

    delete: deleteAt 삭제

--- src/main/java/com/ddang/usedauction/member/dto/MemberGetDto.java
+++ src/main/java/com/ddang/usedauction/member/dto/MemberGetDto.java
@@ -28,45 +28,6 @@ public class MemberGetDto {
     private LocalDateTime createdAt;
     private LocalDateTime deletedAt;
 
-    // deletedAt에 대한 getter 메서드 추가
-    public LocalDateTime getDeletedAt() {
-      return this.deletedAt;
-    }
-
-
-    public String getMemberId() {
-      return memberId;
-    }
-
-    public String getPassWord() {
-      return passWord;
-    }
-
-
-    public String getEmail() {
-      return email;
-    }
-
-    public void setMemberId(String memberId) {
-      this.memberId = memberId;
-    }
-
-    public void setPassWord(String passWord) {
-      this.passWord = passWord;
-    }
-
-    public void setEmail(String email) {
-      this.email = email;
-    }
-    public boolean isValidPassWord(String password) {
-      return this.passWord.equals(password);
-    }
-
-    public Response(String memberId, String passWord) {
-      this.memberId = memberId;
-      this.passWord = passWord;
-    }
-
     // entity -> getResponse
     public static MemberGetDto.Response from(Member member) {
 
@@ -80,7 +41,6 @@ public class MemberGetDto {
               .social(member.getSocial())
               .socialProviderId(member.getSocialProviderId())
               .createdAt(member.getCreatedAt())
-              .deletedAt(member.getDeletedAt())
               .build();
     }
   }
