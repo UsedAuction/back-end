7b576dd7327e85ffb7cbf0d0730db3fe9ac7cf49
sh035 <saab35@naver.com>
2024-08-29 06:37:57 +0900

    feat: 불필요한 필드 제거

--- src/main/java/com/ddang/usedauction/member/dto/MemberLoginResponseDto.java
+++ src/main/java/com/ddang/usedauction/member/dto/MemberLoginResponseDto.java
@@ -1,8 +1,5 @@
 package com.ddang.usedauction.member.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,9 +14,6 @@ public class MemberLoginResponseDto {
 
     String accessToken;
     String memberId;
-
-    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/seoul")
-    private LocalDateTime createdAt;
 
     public static MemberLoginResponseDto from(String accessToken, String memberId) {
         return MemberLoginResponseDto.builder()
