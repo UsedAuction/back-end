1c01d7bd34992f56e210eae443c9de89abf8edad
jinhwanB <paewhal20@gmail.com>
2024-08-30 02:42:47 +0900

    feat: 필터에서 들어오는 토큰 값 확인용 로그 추가

--- src/main/java/com/ddang/usedauction/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/ddang/usedauction/security/jwt/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
@@ -27,6 +29,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         FilterChain filterChain) throws ServletException, IOException {
 
         String accessToken = tokenProvider.resolveTokenFromRequest(request);
+
+        log.info("accessToken = {}", accessToken);
 
         // accessToken 검증
         if (StringUtils.hasText(accessToken)) {
