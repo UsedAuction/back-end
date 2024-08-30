f1ebea82259f29757fc44691a9d14dfdc2aefc08
sh035 <saab35@naver.com>
2024-08-30 20:41:14 +0900

    feat: 채팅방 채팅메시지 조회 기능 구현

--- src/main/java/com/ddang/usedauction/chat/service/ChatMessageService.java
+++ src/main/java/com/ddang/usedauction/chat/service/ChatMessageService.java
@@ -3,11 +3,14 @@ package com.ddang.usedauction.chat.service;
 import com.ddang.usedauction.chat.domain.dto.ChatMessageSendDto;
 import com.ddang.usedauction.chat.domain.entity.ChatMessage;
 import com.ddang.usedauction.chat.domain.entity.ChatRoom;
+import com.ddang.usedauction.chat.exception.UnauthorizedAccessException;
 import com.ddang.usedauction.chat.repository.ChatMessageRepository;
 import com.ddang.usedauction.chat.repository.ChatRoomRepository;
 import com.ddang.usedauction.member.domain.Member;
 import com.ddang.usedauction.member.repository.MemberRepository;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -25,21 +28,44 @@ public class ChatMessageService {
     /**
      * 메시지 저장 Service
      *
-     * @param message (roomId, senderId, message)
+     * @param request (roomId, senderId, message)
      */
     @Transactional
-    public ChatMessage sendMessage(ChatMessageSendDto.Request message) {
-        Member member = memberRepository.findByMemberId(message.getSenderId())
+    public void sendMessage(ChatMessageSendDto.Request request) {
+        Member member = memberRepository.findByMemberId(request.getSenderId())
             .orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다."));
 
-        ChatRoom chatRoom = chatRoomRepository.findById(message.getRoomId())
+        ChatRoom chatRoom = chatRoomRepository.findById(request.getRoomId())
             .orElseThrow(() -> new NoSuchElementException("존재하지 않는 채팅방입니다."));
+
         ChatMessage chatMessage = ChatMessage.builder()
-            .message(message.getMessage())
+            .message(request.getMessage())
             .sender(member)
             .chatRoom(chatRoom)
             .build();
 
-        return chatMessageRepository.save(chatMessage);
+        chatMessageRepository.save(chatMessage);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ChatMessageSendDto.Response> findMessagesByChatRoomId(String memberId,
+        Long chatRoomId) {
+
+        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
+            .orElseThrow(() -> new NoSuchElementException("존재하지 않는 채팅방입니다."));
+
+        // 로그인한 사용자가 채팅방에 속해 있는지 확인
+        if (!isMemberOfChatRoom(chatRoom, memberId)) {
+            throw new UnauthorizedAccessException();
+        }
+
+        return chatMessageRepository.findByChatRoomIdOrderByCreatedAtAsc(chatRoomId).stream()
+            .map(ChatMessageSendDto.Response::from)
+            .collect(Collectors.toList());
+    }
+
+    private boolean isMemberOfChatRoom(ChatRoom chatRoom, String memberId) {
+        return chatRoom.getSeller().getMemberId().equals(memberId) ||
+            chatRoom.getBuyer().getMemberId().equals(memberId);
     }
 }
