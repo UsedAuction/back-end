b21812d0bd227f5b2a3e24520615598ba3c8c3dc
sh035 <saab35@naver.com>
2024-08-31 02:22:26 +0900

    feat: TokenProvider 토큰 추출 메서드 추가

--- src/main/java/com/ddang/usedauction/security/jwt/TokenProvider.java
+++ src/main/java/com/ddang/usedauction/security/jwt/TokenProvider.java
@@ -174,4 +174,13 @@ public class TokenProvider {
 
         return expiration.getTime() - new Date().getTime();
     }
+
+    public String resolveTokenFromHeader(String token) {
+
+        if (!ObjectUtils.isEmpty(token) && token.startsWith(TOKEN_PREFIX)) {
+            return token.substring(TOKEN_PREFIX.length());
+        }
+
+        return null;
+    }
 }
