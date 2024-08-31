be13023227683eea7130261b536943cb8313852c
sh035 <saab35@naver.com>
2024-09-01 02:40:06 +0900

    feat: 채팅방 삭제 기능 구현 - 레디스에 있는 채팅방 정보 삭제 - 채팅방 엔티티에 삭제 날짜 업데이트 - 토픽 삭제

--- src/main/java/com/ddang/usedauction/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/ddang/usedauction/chat/repository/ChatRoomRepository.java
@@ -1,9 +1,12 @@
 package com.ddang.usedauction.chat.repository;
 
 import com.ddang.usedauction.chat.domain.entity.ChatRoom;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
-  boolean existsByAuctionId(Long auctionId);
+    Optional<ChatRoom> findByAuctionId(Long auctionId);
+
+    boolean existsByAuctionId(Long auctionId);
 }
