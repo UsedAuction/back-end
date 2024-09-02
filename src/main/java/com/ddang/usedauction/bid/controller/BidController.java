a203a215809642c11f2ba877f98319ac1ba81558
jinhwanB <paewhal20@gmail.com>
2024-09-03 06:39:48 +0900

    feat: 입찰 내역 조회 시 생성 날짜 기준 내림차순 정렬로 수정

--- src/main/java/com/ddang/usedauction/bid/controller/BidController.java
+++ src/main/java/com/ddang/usedauction/bid/controller/BidController.java
@@ -7,6 +7,7 @@ import com.ddang.usedauction.security.auth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -32,7 +33,7 @@ public class BidController {
     @PreAuthorize("hasRole('USER')")
     @GetMapping
     public ResponseEntity<Page<BidGetDto.Response>> getBidListController(
-        @PageableDefault Pageable pageable,
+        @PageableDefault(sort = "createdAt", direction = Direction.DESC) Pageable pageable,
         @AuthenticationPrincipal PrincipalDetails principalDetails) {
 
         String memberId = principalDetails.getName();
