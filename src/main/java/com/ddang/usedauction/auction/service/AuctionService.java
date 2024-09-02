61f1d68adfda2fb8c675725e45e0df8f151335d5
sh035 <saab35@naver.com>
2024-09-02 22:47:38 +0900

    feat: 즉시구매 후 채팅방 및 채팅 메시지 삭제

--- src/main/java/com/ddang/usedauction/auction/service/AuctionService.java
+++ src/main/java/com/ddang/usedauction/auction/service/AuctionService.java
@@ -23,6 +23,8 @@ import com.ddang.usedauction.auction.repository.AuctionRepository;
 import com.ddang.usedauction.bid.domain.Bid;
 import com.ddang.usedauction.category.domain.Category;
 import com.ddang.usedauction.category.repository.CategoryRepository;
+import com.ddang.usedauction.chat.domain.entity.ChatRoom;
+import com.ddang.usedauction.chat.service.ChatMessageService;
 import com.ddang.usedauction.chat.service.ChatRoomService;
 import com.ddang.usedauction.image.domain.Image;
 import com.ddang.usedauction.image.service.ImageService;
@@ -67,6 +69,7 @@ public class AuctionService {
     private final AuctionRedisService auctionRedisService;
     private final NotificationService notificationService;
     private final ChatRoomService chatRoomService;
+    private final ChatMessageService chatMessageService;
     private final RedisTemplate<String, AuctionRecentDto> redisTemplate;
 
     private static final String RECENTLY_AUCTION_LIST_REDIS_KEY_PREFIX = "recently::";
@@ -266,6 +269,9 @@ public class AuctionService {
 
         // 구매 확정 알림 전송
         sendNotificationForConfirm(buyer, auction);
+
+        ChatRoom chatRoom = chatRoomService.deleteChatRoom(auctionId);
+        chatMessageService.deleteMessagesByChatRoom(chatRoom.getId());
     }
 
     /**
